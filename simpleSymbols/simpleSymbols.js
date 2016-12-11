bd916de2e6f90d72215481273d263018bce84c3b
Andrews Frempong <frempongandrews@yahoo.com>
2016-12-11 22:25:16 +0000

    simpleSymbols solved

---
+++ simpleSymbols/simpleSymbols.js
@@ -0,0 +1,31 @@
+function simpleSymbols (str) {
+
+    var len = str.length;
+    //will use length of letters and result to check if every letter respects the condition
+    var letters = [];
+    var result = [];
+    for (var i = 0; i < len; i++) {
+        //testing if it's a letter
+        if (/[a-z]/gi.test(str[i])) {
+            letters.push(str[i]);
+            if ((str[i+1] === '+') && (str[i-1] === '+')) {
+                result.push(true);
+            }
+        }
+    }
+
+    if (letters.length === result.length) {
+        //if every letter returned true
+        if(result.every(function (el) {
+            return el === true;
+            })) {
+            return true
+        }
+    }
+
+    return false;
+}
+
+console.log(simpleSymbols("+s+t+r+k+"));
+
+//solved
