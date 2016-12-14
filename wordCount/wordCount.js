cd3626c2c6143476e83b46f7e29bdbbbc23c27ad
Andrews Frempong <frempongandrews@yahoo.com>
2016-12-14 22:58:48 +0000

    wordCount done

---
+++ wordCount/wordCount.js
@@ -0,0 +1,8 @@
+function wordCount (str) {
+    var wordsArr = str.split(" ");
+    // console.log(wordsArr);
+    var count = wordsArr.length;
+    console.log(count);
+}
+
+wordCount("Never eat shredded wheat or cake");
