a322faa9141d9a6057aa7b454e0d284c83707c7e
Andrews Frempong <frempongandrews@yahoo.com>
2016-12-15 09:49:53 +0000

    arithGeo solved

--- arithGeo/arithGeo.js
+++ arithGeo/arithGeo.js
@@ -1,5 +1,47 @@
-function arithGeo () {
+function arithGeo(arr) {
+    var aritArr = [];
+    var geoArr = [];
+    var len = arr.length;
+
+    for (var i = 1; i < len; i++) {
+        //getting the difference between all values
+        //if same ---> Arithmetic
+        var difference = arr[i] - arr[i - 1];
+        aritArr.push(difference);
+
+        //getting division between all values
+        //if same ---> Geometric
+        var divi = arr[i] / arr[i - 1];
+        geoArr.push(divi);
+    }
+
+    //function that checks if all values are same
+    Array.prototype.allSameValues = function () {
+        var ln = this.length;
+        for (var j = 0; j < ln; j++) {
+            if (this[j] !== this[0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    if (aritArr.allSameValues()) {
+        console.log("Aritmetic");
+    }
+    if (geoArr.allSameValues()) {
+        console.log("Geometric");
+    }
+
+    if (!aritArr.allSameValues() && !geoArr.allSameValues()) {
+        console.log(-1);
+    }
 
 }
 
-arithGeo([2,4,6,8]);
+arithGeo([2, 4, 6, 8]);
+arithGeo([2, 6, 18, 54]);
+
+
+//solved
